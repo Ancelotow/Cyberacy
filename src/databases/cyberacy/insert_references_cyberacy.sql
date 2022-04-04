1c3e5252f78c05ad1527c6df525167600916f916
ANCELOT Owen <ancelotow@gmail.com>
2022-04-04 22:20:08 +0200

    SQL scripts finished

---
+++ src/databases/cyberacy/insert_references_cyberacy.sql
@@ -0,0 +1,62 @@
+do
+$sex$
+begin
+    if not exists (select * from sex where sex_id = 1) then
+        insert into sex(sex_id, sex_name) values (1, 'Homme');
+    end if;
+
+    if not exists (select * from sex where sex_id = 2) then
+        insert into sex(sex_id, sex_name) values (2, 'Femme');
+    end if;
+end;
+$sex$;
+
+do
+$type_vote$
+begin
+    if not exists (select * from type_vote where tvo_id = 1) then
+        insert into type_vote(tvo_id, tvo_name) values (1, 'Présidentiel');
+    end if;
+
+    if not exists (select * from type_vote where tvo_id = 2) then
+        insert into type_vote(tvo_id, tvo_name) values (2, 'Régional');
+    end if;
+
+    if not exists (select * from type_vote where tvo_id = 3) then
+        insert into type_vote(tvo_id, tvo_name) values (3, 'Départemental');
+    end if;
+
+    if not exists (select * from type_vote where tvo_id = 4) then
+        insert into type_vote(tvo_id, tvo_name) values (4, 'Municipal');
+    end if;
+
+    if not exists (select * from type_vote where tvo_id = 5) then
+        insert into type_vote(tvo_id, tvo_name) values (5, 'Législative');
+    end if;
+
+    if not exists (select * from type_vote where tvo_id = 6) then
+        insert into type_vote(tvo_id, tvo_name) values (6, 'Référundum');
+    end if;
+end;
+$type_vote$;
+
+do
+$type_step$
+begin
+    if not exists (select * from type_step where tst_id = 1) then
+        insert into type_step(tst_id, tst_name) values (1, 'Départ');
+    end if;
+
+    if not exists (select * from type_step where tst_id = 2) then
+        insert into type_step(tst_id, tst_name) values (2, 'Etape');
+    end if;
+
+    if not exists (select * from type_step where tst_id = 3) then
+        insert into type_step(tst_id, tst_name) values (3, 'Arrivée');
+    end if;
+end;
+$type_step$;
+
+
+
+
