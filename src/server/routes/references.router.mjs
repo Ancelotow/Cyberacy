378a1c04736545160c2ec1f9148bcedc4bd82eec
ANCELOT Owen <ancelotow@gmail.com>
2022-04-23 09:36:32 +0200

    Rename endpoint "/step_type" to "/type_step"

--- src/server/routes/references.router.mjs
+++ src/server/routes/references.router.mjs
@@ -3,7 +3,7 @@ import express from "express"
 
 const routerRef = express.Router()
 
-routerRef.get("/step_type", async (req, res) => {
+routerRef.get("/type_step", async (req, res) => {
     // #swagger.tags = ['References']
     // #swagger.description = 'Récupération des types d'étapes pour le trajet des manifestations.'
     // #swagger.security = [{ "Bearer": [] }]
