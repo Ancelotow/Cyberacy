207f2dee8686ef0c92263a47fe64fdc21ae040e5
Ancelotow <ancelotow@gmail.com>
2022-06-22 10:21:51 +0200

    .build: ad platform Android & IOS

--- src/client/flutter/lib/models/session.dart
+++ src/client/flutter/lib/models/session.dart
@@ -1,11 +1,18 @@
 import 'package:bo_cyberacy/models/services/auth_service.dart';
 import 'package:hive/hive.dart';
+import 'dart:io' show Platform;
+
+import 'package:path_provider/path_provider.dart';
 
 class Session {
 
   static const String _idHiveToken = "JWTToken";
 
   static Future<void> initToken(String nir, String pwd) async {
+    if(Platform.isAndroid || Platform.isIOS) {
+      var dir = await getApplicationDocumentsDirectory();
+      Hive.init(dir.path);
+    }
     Box<String> box = await Hive.openBox("loginBox");
     var token = await AuthService().login(nir, pwd);
     box.put(_idHiveToken, token);
