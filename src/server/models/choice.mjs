2ba2ca4af68a8a89e093eae5d660367089a2d387
Ancelotow <ancelotow@gmail.com>
2022-07-14 03:54:50 +0200

    .fix: add choice with color

--- src/server/models/choice.mjs
+++ src/server/models/choice.mjs
@@ -21,7 +21,7 @@ class Choice{
 Choice.prototype.Add = function() {
     return new Promise((resolve, reject) => {
         const request = {
-            text: 'INSERT INTO choice (cho_name, vte_id, cho_description, prs_nir, clr_id) VALUES ($1, $2, $3, $4)',
+            text: 'INSERT INTO choice (cho_name, vte_id, cho_description, prs_nir, clr_id) VALUES ($1, $2, $3, $4, $5)',
             values: [this.name, this.id_vote, this.description, this.candidat_nir, this.id_color],
         }
         pool.query(request, (error, _) => {
