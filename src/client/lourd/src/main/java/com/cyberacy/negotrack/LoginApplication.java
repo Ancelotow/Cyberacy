78ad22d1669c2bcae220a94d52c6e457b174474e
ANCELOT Owen <ancelotow@gmail.com>
2022-07-15 12:30:48 +0200

    .feat: login page

--- src/client/lourd/src/main/java/com/cyberacy/negotrack/LoginApplication.java
+++ src/client/lourd/src/main/java/com/cyberacy/negotrack/LoginApplication.java
@@ -4,9 +4,11 @@ import com.cyberacy.negotrack.models.ConnectDB;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class LoginApplication extends Application {
 
@@ -14,7 +16,9 @@ public class LoginApplication extends Application {
     public void start(Stage stage) throws IOException {
         new ConnectDB().connect();
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
+        Image ico = new Image(LoginApplication.class.getResource("images/negotrack_icone.png").toString());
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.getIcons().add(ico);
         stage.setTitle("Negotrack : Connexion");
         stage.setScene(scene);
         stage.show();
