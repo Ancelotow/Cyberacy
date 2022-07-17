2f67bf0f6a5ad34e0445b8de23f0190bdd9a8baf
ANCELOT Owen <ancelotow@gmail.com>
2022-07-17 20:37:12 +0200

    .feat: return to Java 11

---
+++ src/client/lourd/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+module com.cyberacy.negotrack {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.sql;
+
+    opens com.cyberacy.negotrack to javafx.fxml;
+    opens com.cyberacy.negotrack.views to javafx.fxml;
+    exports com.cyberacy.negotrack;
+    exports com.cyberacy.negotrack.models;
+    exports com.cyberacy.negotrack.models.entities;
+    exports com.cyberacy.negotrack.models.exceptions;
+    exports com.cyberacy.negotrack.views;
+    exports com.cyberacy.negotrack.views.modals.modal_message;
+    exports com.cyberacy.negotrack.views.modals.add_project;
+    exports com.cyberacy.negotrack.views.modals.add_user_story;
+    exports com.cyberacy.negotrack.views.modals.add_epic;
+    exports com.cyberacy.negotrack.views.modals.add_task;
+}
