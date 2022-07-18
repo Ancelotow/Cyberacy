399395c8bd471daea99d4a19e4962fd4596c3bf0
Ancelotow <31661146+Ancelotow@users.noreply.github.com>
2022-07-19 00:36:34 +0200

    .feat: add message when no vote

--- src/app/android/app/src/main/java/com/cyberacy/app/ui/vote/VoteFragment.kt
+++ src/app/android/app/src/main/java/com/cyberacy/app/ui/vote/VoteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,18 +29,24 @@ class VoteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val shimmerLayout = view.findViewById<ShimmerFrameLayout>(R.id.shimmer_layout)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
+        val labelNoData = view.findViewById<TextView>(R.id.label_no_data)
         recyclerView.visibility = View.GONE
         viewModel.listVotes.observe(viewLifecycleOwner) {
             when (it) {
                 is VoteStateError -> {
                     shimmerLayout.visibility = View.GONE
+                    labelNoData.visibility = View.GONE
                 }
                 VoteStateLoading -> {
                     recyclerView.visibility = View.GONE
                     shimmerLayout.visibility = View.VISIBLE
+                    labelNoData.visibility = View.GONE
                 }
                 is VoteStateSuccess -> {
                     shimmerLayout.visibility = View.GONE
+                    if(it.votes.isEmpty()) {
+                        labelNoData.visibility = View.VISIBLE
+                    }
                     recyclerView.visibility = View.VISIBLE
                     recyclerView.adapter = ListAdapterVote(it.votes as MutableList<Vote>, this.activity)
                     recyclerView.layoutManager = GridLayoutManager(context, 1)
